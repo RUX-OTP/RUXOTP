9070dc858aa8da05183a2ce9ce0e6640624a085f
RUX-OTP <w4025350@gmail.com>
2025-09-22 16:12:52 +0100

    Update page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,1 +1,10 @@
+// app/auth/login/page.tsx
+import AuthForm from "@/src/components/AuthForm";
 
+export default function LoginPage() {
+  return (
+    <section className="py-12">
+      <AuthForm type="login" />
+    </section>
+  );
+}
