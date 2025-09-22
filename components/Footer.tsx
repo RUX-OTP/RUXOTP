da8897be0afd30103d1684909043699d9f104015
RUX-OTP <w4025350@gmail.com>
2025-09-22 15:51:34 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,1 +1,9 @@
-
+export default function Footer() {
+  return (
+    <footer className="bg-gray-100 text-center py-6 mt-12 border-t">
+      <p className="text-gray-600">
+        &copy; {new Date().getFullYear()} RuxOTP. All rights reserved.
+      </p>
+    </footer>
+  );
+      }
