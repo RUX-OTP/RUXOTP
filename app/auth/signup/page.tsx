f3203c485f63d6151c9ba2fa7d7f15eb40e1a065
RUX-OTP <w4025350@gmail.com>
2025-09-22 16:13:32 +0100

    Update page.tsx

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,1 +1,10 @@
+// app/auth/signup/page.tsx
+import AuthForm from "@/src/components/AuthForm";
 
+export default function SignupPage() {
+  return (
+    <section className="py-12">
+      <AuthForm type="signup" />
+    </section>
+  );
+}
